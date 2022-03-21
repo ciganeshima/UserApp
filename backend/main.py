f57541dbf1b375773b2915c3b8c2e4fd077d8828
19333690 <DSSenglyaev@sberbank.ru>
2022-03-21 14:52:40 +0300

    add database seed, add env file

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from sqlalchemy import event
+
 from core.config import settings
 from db.session import engine
 from db.base import Base
+from db.models.users import Users
 from apis.base import api_router
 
 
@@ -19,10 +22,19 @@ def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
+# This method receives a table, a connection and inserts data to that table.
+def initialize_table(target, connection, **kw):
+    table_name = str(target)
+    if table_name in settings.INITIAL_DATA and len(settings.INITIAL_DATA[table_name]) > 0:
+        connection.execute(target.insert(), settings.INITIAL_DATA[table_name])
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
+    # set up this event before table creation
+    event.listen(Users.__table__, 'after_create', initialize_table)
     create_tables()
     return app
 
